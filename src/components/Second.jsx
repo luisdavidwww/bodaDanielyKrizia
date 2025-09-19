3c6801a0a1df4affe920c987dc798f6044cb6d2e
Luis Torrealba <luisdavid25torrealba@gmail.com>
2025-09-19 00:45:36 -0400

    editado

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -9,7 +9,7 @@ const Second = () => {
        <div className='containerbtn-ulti'>
           <a className='btn-outline-ulti' type='button' href='https://walink.co/66c619'> Confirma la Asistencia </a>
            <a className='btn-outline-ulti' type='button' href='https://maps.app.goo.gl/E8rnPFYNynm6sbcf6'> Ubicación </a>
-           <a className='btn-outline-ulti' type='button' href='https://sites.google.com/view/daniel-krizia/p%C3%A1gina-principal?authuser=2'> Referencia Dess Code <br /> <span style={{ fontSize:'0.7rem', fontWeight:'100'}}>No Usar colores Blanco y Beige</span></a>
+           <a className='btn-outline-ulti' type='button' href='https://sites.google.com/view/daniel-krizia/p%C3%A1gina-principal?authuser=2'> Referencia Dess Code <br /> <span style={{ fontSize:'0.7rem', fontWeight:'100'}}>(No Usar colores Blanco y Beige)</span></a>
            <a className='btn-outline-ulti' type='button' href='https://web.dotstheapp.com/a?group=2145908&code=g0owkHy6&dlBy=louisdetalles00&utm_source=guest&utm_medium=share&utm_campaign=guest_event_album&force_app=1'> Comparte las fotos <br /> <span style={{ fontSize:'0.7rem', fontWeight:'100'}}> de nuestra boda </span> </a>
             <a className='btn-outline-ulti' type='button' href='https://www.amazon.com/wedding/share/BodaDanielyKrizia'> Opcion de regalos </a>
       </div>
